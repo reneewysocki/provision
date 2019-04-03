9860d3108bd6ad52e2e862483fd9b7c0630d0f97
Renee Wysocki <reneew311@gmail.com>
2019-04-03 10:39:51 -0500

    text update

--- php/mail.php
+++ php/mail.php
@@ -1,14 +1,18 @@
 <?php
+if(isset( $_POST['company']))
+$company = $_POST['company'];
+if(isset( $_POST['priority']))
+$priority = $_POST['priority'];
 if(isset( $_POST['fname']))
-$name = $_POST['fame'];
+$fname = $_POST['fname'];
 if(isset( $_POST['lname']))
-$name = $_POST['lname'];
+$lname = $_POST['lname'];
+if(isset( $_POST['phone']))
+$phone = $_POST['phone'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
 if(isset( $_POST['message']))
 $message = $_POST['message'];
-if(isset( $_POST['company']))
-$company = $_POST['company'];
 
 $content="From: $name \n Email: $email \n Message: $message";
 $recipient = "reneew311@gmail.com";
